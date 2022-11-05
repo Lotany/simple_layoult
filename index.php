008f55b2694e108a7cf272d502e9117542dbb570
Able <ablekilinda@gmail.com>
2022-11-05 09:35:21 +0300

    adding functions

--- index.php
+++ index.php
@@ -12,26 +12,41 @@
     <div class="header">Lotans</div>
 
     <div class="row">
+
+        <!--left column -->
         <div class="leftcolumn">
             <div class="card">
-                <h2>
-                    <Title> heading</Title>
-                </h2>
+                <h2> heading</h2>
                 <h5>Tittle description, dec 5, 2020</h5>
                 <img src="images/image_3.png" class="fakeimg" style="height: 200px;">
                 <p>some text</p>
             </div>
-
             <div class="card">
-                <h2>
-                    <Title> heading 2</Title>
-                </h2>
+                <h2>heading 2</h2>
                 <h5>Tittle description, dec 5, 2020</h5>
                 <img src="images/image_3.png" class="fakeimg" style="height: 200px;">
                 <p>some text</p>
             </div>
         </div>
 
+        <!-- middle column-->
+        <div class="middlecolumn">
+            <div class="card">
+                <h2> heading</h2>
+                <h5>Tittle description, dec 5, 2020</h5>
+                <img src="images/image_3.png" class="fakeimg" style="height: 200px;">
+                <p>some text</p>
+            </div>
+            <div class="card">
+                <h2>heading 2</h2>
+                <h5>Tittle description, dec 5, 2020</h5>
+                <img src="images/image_3.png" class="fakeimg" style="height: 200px;">
+                <p>some text</p>
+            </div>
+        </div>
+
+
+        <!--right column -->
         <div class="rightcolumn">
             <div class="card">
                 <h2>About me</h2>
