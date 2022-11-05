572898abc964a630f67a665b8623fd0b610d65f0
Able <ablekilinda@gmail.com>
2022-11-05 03:46:55 +0300

    lotan

--- index.php
+++ index.php
@@ -8,6 +8,47 @@
 
     <div class="header">Lotans</div>
 
+    <div class=" row">
+        <div class="leftcolumn">
+            <div class="card">
+                <h2><Title> heading</Title></h2>
+                <h5>Tittle description, dec 5, 2020</h5>
+                <div class="fakeimg" style="height: 200px;">image</div>
+                <p>some text</p>
+            </div>
+
+            <div class="card">
+                <h2><Title> heading 2</Title></h2>
+                <h5>Tittle description, dec 5, 2020</h5>
+                <div class="fakeimg" style="height: 200px;">image</div>
+                <p>some text</p>
+            </div>
+        </div>
+
+        <div class="rightcolumn">
+
+        <div class="card">
+            <h2>About me</h2>
+            <div class="fakeimg" style="height: 100px;">Image</div>
+            <p>Some text</p>
+        </div>
+            <div class="card">
+                <h3>Popular Posts</h3>
+                <div class="fakeimg"><br>Image</div>
+                <div class="fakeimg"><br>Image</div>
+                <div class="fakeimg"><br>Image</div>
+            </div>
+
+            <div class="card">
+                <h3>Follow Me</h3>
+                <p>Some text</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <h2>Footer</h2>
+    </div>
         
     </body>
 </html>
