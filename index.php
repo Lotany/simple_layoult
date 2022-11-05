0472191ac2c2fe81ab88388df109c753f04acce5
Able <ablekilinda@gmail.com>
2022-11-05 03:27:34 +0300

    adding tyhe body

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>simple blog layoult</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+
+    <div class="header">Lotans</div>
+
+        
+    </body>
+</html>
